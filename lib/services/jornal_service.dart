27b3da0160e7507163c883d38fc6debb16bc2b3b
Leonardo <leoruizz14@outlook.com>
2023-10-12 11:35:26 -0300

    putting client in the API, to use interceptors

--- lib/services/jornal_service.dart
+++ lib/services/jornal_service.dart
@@ -1,19 +1,24 @@
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
 
 class JournalService {
   static const String url = "http://192.168.15.15:3000/";
   static const String resource = "learnhttp/";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]); //passamos um client para o http, para assim ter o acesso aos interceptadores quando uma requisição ou resposta for feita no servidor.
 
   String getUrl() {
     return "$url$resource";
   }
 
   register(String content) {
-    http.post(Uri.parse(getUrl()), body: {"content": content});
+    client.post(Uri.parse(getUrl()), body: {"content": content});
   }
 
   Future<String> get() async {
-    http.Response response = await http.get(Uri.parse(getUrl()));
+    http.Response response = await client.get(Uri.parse(getUrl()));
     print(response.body);
     return response.body;
   }
