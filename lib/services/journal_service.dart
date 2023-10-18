71646f2f59f4f25dba2ad17498f99e4c7234983c
Leonardo <leoruizz14@outlook.com>
2023-10-18 16:44:27 -0300

    update resource and modify register method

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter_webapi_first_course/models/journal.dart';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
 
 class JournalService {
   static const String url = "http://192.168.15.15:3000/";
-  static const String resource = "learnhttp/";
+  static const String resource = "journals/";
 
   http.Client client = InterceptedClient.build(interceptors: [
     LoggingInterceptor()
@@ -14,13 +17,23 @@ class JournalService {
     return "$url$resource";
   }
 
-  register(String content) {
-    client.post(Uri.parse(getUrl()), body: {"content": content});
+  Future<bool> register(Journal journal) async {
+    String jsonJournal =
+        json.encode(journal.toMap()); //json encode converte o map em json
+    http.Response response = await client.post(
+      Uri.parse(getUrl()),
+      headers: {'Content-type': 'application/json'},
+      body: jsonJournal,
+    );
+    if (response.statusCode == 201) {
+      return true;
+    }
+    return false;
   }
 
   Future<String> get() async {
     http.Response response = await client.get(Uri.parse(getUrl()));
-    print(response.body);
+    // print(response.body);
     return response.body;
   }
 }
