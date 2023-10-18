581aefbf72694f25aa1413c0a132cbe1fe0cbbca
Leonardo <leoruizz14@outlook.com>
2023-10-18 16:46:01 -0300

    update add_journal_screen

--- lib/screens/add_journal_screen/add_journal_screen.dart
+++ lib/screens/add_journal_screen/add_journal_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webapi_first_course/helpers/weekday.dart';
 import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
 
 class AddJournalScreen extends StatelessWidget {
+  //CRIAÇÃO DA TELA PÓS CLIQUE NO CARD
   final Journal journal;
-  const AddJournalScreen({super.key, required this.journal});
+  AddJournalScreen({super.key, required this.journal});
+
+  final TextEditingController _contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +16,34 @@ class AddJournalScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(
             '${WeekDay(journal.createdAt.weekday).long.toLowerCase()}  -  ${journal.createdAt.day}  |  ${journal.createdAt.month}  |  ${journal.createdAt.year}'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.check))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                registerJournal();
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.check))
+        ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: TextField(
+          controller: _contentController,
           keyboardType: TextInputType.multiline,
-          style: TextStyle(fontSize: 24),
+          style: const TextStyle(fontSize: 24),
           expands: true,
           minLines: null,
           maxLines: null,
         ),
       ), //.long porque queremos a data longa (Ex: ao invés de 'dom' será 'domingo')
     );
+  }
+
+  registerJournal() {
+    String content = _contentController.text;
+    journal.content =
+        content; //atualiza o valor de journal que era vazio, agora passa a ter um valor
+    JournalService service = JournalService();
+    service.register(journal);
   }
 }
