a623c484bb5fb19120ac8caff6a763cae4e3a609
Leonardo <leoruizz14@outlook.com>
2023-10-25 15:02:09 -0300

    update with edit and delete methods

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -4,6 +4,7 @@ import 'package:flutter_webapi_first_course/models/journal.dart';
 import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_interceptor/http/http.dart';
+import 'package:uuid/uuid.dart';
 
 class JournalService {
   static const String url = "http://192.168.15.15:3000/";
@@ -20,6 +21,7 @@ class JournalService {
   Future<bool> register(Journal journal) async {
     String jsonJournal =
         json.encode(journal.toMap()); //json encode converte o map em json
+    // int userId = int.parse(const Uuid().v1());
     http.Response response = await client.post(
       Uri.parse(getUrl()),
       headers: {'Content-type': 'application/json'},
@@ -43,5 +45,28 @@ class JournalService {
       list.add(Journal.fromMap(jsonMap));
     }
     return list;
+  }
+
+  Future<bool> editJournal(String id, Journal journal) async {
+    String jsonJournal = json.encode(
+      journal.toMap(),
+    );
+    http.Response response = await client.put(
+      Uri.parse("${getUrl()}$id"),
+      headers: {'Content-type': 'application/json'},
+      body: jsonJournal,
+    ); //json encode converte o map em json
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> deleteJournal(String id) async {
+    http.Response response = await http.delete(Uri.parse('${getUrl()}$id'));
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
   }
 }
