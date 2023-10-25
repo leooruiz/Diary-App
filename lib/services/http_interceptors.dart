346411af962e1d80f7c3ad817675324eb55589b8
Leonardo <leoruizz14@outlook.com>
2023-10-25 14:56:01 -0300

    server updates

--- lib/services/http_interceptors.dart
+++ lib/services/http_interceptors.dart
@@ -5,7 +5,7 @@ class LoggingInterceptor implements InterceptorContract {
   Logger logger = Logger();
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
-      logger.i(
+    logger.i(
         "Requisição de ${data.baseUrl}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
     return data;
   }
@@ -14,12 +14,11 @@ class LoggingInterceptor implements InterceptorContract {
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
     if (data.statusCode ~/ 100 == 2) {
       logger.i(
-        "Resposta de ${data.url}\nStatus da Resposta: ${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
-    }else{
+          "Resposta de ${data.url}\nStatus da Resposta: ${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+    } else {
       logger.e(
-        "Resposta de ${data.url}\nStatus da Resposta: ${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+          "Resposta de ${data.url}\nStatus da Resposta: ${data.statusCode}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
     }
     return data;
-    
   }
-  }
+}
