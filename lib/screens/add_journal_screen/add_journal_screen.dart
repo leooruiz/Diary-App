b0dfdd726bc640d4bd295d1e0a711ad31c0b6421
Leonardo <leoruizz14@outlook.com>
2023-10-25 15:00:32 -0300

    add isEditing variable to edit cars

--- lib/screens/add_journal_screen/add_journal_screen.dart
+++ lib/screens/add_journal_screen/add_journal_screen.dart
@@ -6,12 +6,13 @@ import 'package:flutter_webapi_first_course/services/journal_service.dart';
 class AddJournalScreen extends StatelessWidget {
   //CRIAÇÃO DA TELA PÓS CLIQUE NO CARD
   final Journal journal;
-  AddJournalScreen({super.key, required this.journal});
-
+  final bool isEditing;
+  AddJournalScreen({super.key, required this.journal, required this.isEditing});
   final TextEditingController _contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    _contentController.text = journal.content;
     return Scaffold(
       appBar: AppBar(
         title: Text(WeekDay(journal.createdAt).toString()),
@@ -33,7 +34,7 @@ class AddJournalScreen extends StatelessWidget {
           minLines: null,
           maxLines: null,
         ),
-      ), //.long porque queremos a data longa (Ex: ao invés de 'dom' será 'domingo')
+      ), //.long criado em Weekday.dart porque queremos a data longa (Ex: ao invés de 'dom' será 'domingo')
     );
   }
 
@@ -42,7 +43,15 @@ class AddJournalScreen extends StatelessWidget {
     journal.content =
         content; //atualiza o valor de journal que era vazio, agora passa a ter um valor
     JournalService service = JournalService();
-    bool result = await service.register(journal);
-    Navigator.pop(context, result); //é possível enviar um argumento pelo Navigator, que pode ser usado por quem chamou a tela, que neste caso é o journal_card
+    if (isEditing) {
+      service.register(journal).then((value) {
+        Navigator.pop(context, value);
+      });
+    } else {
+      service.editJournal(journal.id, journal).then((value) {
+        Navigator.pop(context,
+            value); //é possível enviar um argumento pelo Navigator, que pode ser usado por quem chamou a tela, que neste caso é o journal_card
+      });
+    }
   }
 }
