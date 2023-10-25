df30446bd8f3b67f592754e88152edb0cf146397
Leonardo <leoruizz14@outlook.com>
2023-10-25 15:00:57 -0300

    comment refresh for while

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -53,12 +53,12 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void refresh() async {
-    List<Journal> listJournal = await service.getAll();
-    setState(() {
-      database = {};
-      for (Journal journal in listJournal) {
-        database[journal.id] = journal;
-      }
-    });
+  //   List<Journal> listJournal = await service.getAll();
+  //   setState(() {
+  //     database = {};
+  //     for (Journal journal in listJournal) {
+  //       database[journal.id] = journal;
+  //     }
+  //   });
   }
 }
