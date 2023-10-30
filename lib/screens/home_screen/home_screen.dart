6183c52be86e70934e44a4f22f2f3fc03b38189c
Leonardo <leoruizz14@outlook.com>
2023-10-30 16:45:23 -0300

    creating Drawer and update refresh function

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/helpers/logout.dart';
+import 'package:flutter_webapi_first_course/screens/commom/exception_dialog.dart';
 import 'package:flutter_webapi_first_course/screens/home_screen/widgets/home_screen_list.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../models/journal.dart';
 
@@ -25,6 +30,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   JournalService service = JournalService();
 
+  int? userId;
+
   @override
   void initState() {
     refresh();
@@ -34,31 +41,73 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Título basado no dia atual
-        title: Text(
-          "${currentDay.day}  |  ${currentDay.month}  |  ${currentDay.year}",
+        appBar: AppBar(
+          // Título basado no dia atual
+          title: Text(
+            "${currentDay.day}  |  ${currentDay.month}  |  ${currentDay.year}",
+          ),
         ),
-      ),
-      body: ListView(
-        controller: _listScrollController,
-        children: generateListJournalCards(
-          windowPage: windowPage,
-          currentDay: currentDay,
-          database: database,
-          refreshFunction: refresh,
-        ),
-      ),
-    );
+        drawer: Drawer(
+            child: ListView(padding: EdgeInsets.zero, children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black38,
+            ),
+            child: SizedBox(),
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              logout(context);
+            },
+          ),
+        ])),
+        body: (userId != null)
+            ? ListView(
+                controller: _listScrollController,
+                children: generateListJournalCards(
+                  windowPage: windowPage,
+                  currentDay: currentDay,
+                  database: database,
+                  refreshFunction: refresh,
+                  userId: userId!,
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ));
   }
 
   void refresh() async {
-  //   List<Journal> listJournal = await service.getAll();
-  //   setState(() {
-  //     database = {};
-  //     for (Journal journal in listJournal) {
-  //       database[journal.id] = journal;
-  //     }
-  //   });
+    SharedPreferences.getInstance().then(
+      (prefs) {
+        String? token = prefs.getString('accessToken');
+        int? id = prefs.getInt('id');
+        String? email = prefs.getString('email');
+        if (token != null && email != null && id != null) {
+          setState(() {
+            userId = id;
+          });
+          service
+              .getAll(id: id.toString(), token: token)
+              .then((List<Journal> listJournal) {
+            setState(() {
+              database = {};
+              for (Journal journal in listJournal) {
+                database[journal.id] = journal;
+              }
+            });
+          });
+        } else {
+          Navigator.pushReplacementNamed(context, 'login');
+        }
+      },
+    ).catchError((error) {
+      logout(context);
+    }, test: (error) => error is TokenNotValidException).catchError((error) {
+      var innerError = error as HttpException;
+      showExceptionDialog(context, content: innerError.message);
+    }, test: (error) => error is HttpException);
   }
 }
