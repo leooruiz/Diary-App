cb194aea6dfd85dba46fc3e671ef5cb0ef04ed10
Leonardo <leoruizz14@outlook.com>
2023-10-30 16:50:54 -0300

    update journal service

--- lib/services/journal_service.dart
+++ lib/services/journal_service.dart
@@ -1,42 +1,46 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter_webapi_first_course/models/journal.dart';
-import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
+import 'package:flutter_webapi_first_course/services/webclient.dart';
 import 'package:http/http.dart' as http;
-import 'package:http_interceptor/http/http.dart';
-import 'package:uuid/uuid.dart';
 
 class JournalService {
-  static const String url = "http://192.168.15.15:3000/";
   static const String resource = "journals/";
-
-  http.Client client = InterceptedClient.build(interceptors: [
-    LoggingInterceptor()
-  ]); //passamos um client para o http, para assim ter o acesso aos interceptadores quando uma requisição ou resposta for feita no servidor.
+  String url = WebClient.url;
+  http.Client client = WebClient().client;
 
   String getUrl() {
     return "$url$resource";
   }
 
-  Future<bool> register(Journal journal) async {
+  Future<bool> register(Journal journal, String token) async {
     String jsonJournal =
         json.encode(journal.toMap()); //json encode converte o map em json
-    // int userId = int.parse(const Uuid().v1());
     http.Response response = await client.post(
       Uri.parse(getUrl()),
-      headers: {'Content-type': 'application/json'},
+      headers: {
+        'Content-type': 'application/json',
+        "Authorization": "Bearer $token"
+      },
       body: jsonJournal,
     );
-    if (response.statusCode == 201) {
-      return true;
+    if (response.statusCode != 201) {
+      if (response.body == 'jwt expired') {
+        throw TokenNotValidException();
+      }
+      throw HttpException(response.body);
     }
-    return false;
+    return true;
   }
 
-  Future<List<Journal>> getAll() async {
-    http.Response response = await client.get(Uri.parse(getUrl()));
+  Future<List<Journal>> getAll(
+      {required String id, required String token}) async {
+    http.Response response = await client.get(
+        Uri.parse("${url}users/$id/journals"),
+        headers: {"Authorization": "Bearer $token"});
     if (response.statusCode != 200) {
-      throw Exception();
+      throw UnauthorizedUserException();
     }
     List<Journal> list = [];
     List<dynamic> listDynamic = json.decode(response.body);
@@ -47,26 +51,41 @@ class JournalService {
     return list;
   }
 
-  Future<bool> editJournal(String id, Journal journal) async {
+  Future<bool> editJournal(String id, Journal journal, String token) async {
+    journal.updatedAt = DateTime.now();
     String jsonJournal = json.encode(
       journal.toMap(),
     );
     http.Response response = await client.put(
       Uri.parse("${getUrl()}$id"),
-      headers: {'Content-type': 'application/json'},
+      headers: {
+        'Content-type': 'application/json',
+        "Authorization": "Bearer $token"
+      },
       body: jsonJournal,
     ); //json encode converte o map em json
-    if (response.statusCode == 200) {
-      return true;
+    if (response.statusCode != 200) {
+      if (response.body == 'jwt expired') {
+        throw TokenNotValidException();
+      }
+      throw HttpException(response.body);
     }
-    return false;
+    return true;
   }
 
-  Future<bool> deleteJournal(String id) async {
-    http.Response response = await http.delete(Uri.parse('${getUrl()}$id'));
-    if (response.statusCode == 200) {
-      return true;
+  Future<bool> deleteJournal(String id, String token) async {
+    http.Response response = await http.delete(Uri.parse('${getUrl()}$id'),
+        headers: {"Authorization": "Bearer $token"});
+    if (response.statusCode != 200) {
+      if (response.body == 'jwt expired') {
+        throw TokenNotValidException();
+      }
+      throw HttpException(response.body);
     }
-    return false;
+    return true;
   }
 }
+
+class UnauthorizedUserException implements Exception {}
+
+class TokenNotValidException implements Exception {}
