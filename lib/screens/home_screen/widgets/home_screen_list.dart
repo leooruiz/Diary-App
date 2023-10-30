43b04d2d2b31322dc90dffb755cf2b92bee47209
Leonardo <leoruizz14@outlook.com>
2023-10-30 16:46:02 -0300

    add userId to home_screen

--- lib/screens/home_screen/widgets/home_screen_list.dart
+++ lib/screens/home_screen/widgets/home_screen_list.dart
@@ -5,13 +5,15 @@ List<JournalCard> generateListJournalCards(
     {required int windowPage,
     required DateTime currentDay,
     required Map<String, Journal> database,
-    required Function refreshFunction}) {
+    required Function refreshFunction,
+    required int userId}) {
   // Cria uma lista de Cards vazios
   List<JournalCard> list = List.generate(
     windowPage + 1,
     (index) => JournalCard(
       showedDate: currentDay.subtract(Duration(days: (windowPage) - index)),
       refreshFunction: refreshFunction,
+      userId: userId,
     ),
   );
 
@@ -28,6 +30,7 @@ List<JournalCard> generateListJournalCards(
         showedDate: list[difference].showedDate,
         journal: value,
         refreshFunction: refreshFunction,
+        userId: userId,
       );
     }
   });
